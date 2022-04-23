b63d7be68f6b9a5801bb230083efa91cf600e932
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-04-23 19:06:34 +0300

    feat: made rollup preserve the original structure

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,17 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 import { peerDependencies, dependencies } from './package.json';
 
+const externalPackages = [
+  ...Object.keys(dependencies || {}),
+  ...Object.keys(peerDependencies || {}),
+];
+
+// Creating regexes of the packages to make sure subpaths of the
+// packages are also treated as external
+const regexesOfPackages = externalPackages.map(
+  (packageName) => new RegExp(`^${packageName}(/.*)?`),
+);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -40,11 +51,15 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src', 'index.ts'),
       formats: ['es', 'cjs'],
-      fileName: (ext) => `index.${ext}.js`,
-      // for UMD name: 'GlobalName'
     },
     rollupOptions: {
-      external: [...Object.keys(peerDependencies), ...Object.keys(dependencies)],
+      output: {
+        dir: 'dist/',
+        format: 'es',
+        preserveModules: true,
+        entryFileNames: ({ name: fileName }) => `${fileName}.js`,
+      },
+      external: regexesOfPackages,
     },
     target: 'esnext',
     sourcemap: true,
