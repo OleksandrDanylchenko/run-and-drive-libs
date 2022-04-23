84c0782f3ccd78892228d93231c6b26212f8976d
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-04-23 20:55:47 +0300

    feat: add hooks to exported members

---
+++ src/hooks/index.ts
@@ -0,0 +1,1 @@
+export { default as useEventRef } from './useEventRef';
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export * as components from './components';
 export * as models from './models';
 export * as utils from './utils';
+export * as hooks from './hooks';
 export * as styles from './styles';
