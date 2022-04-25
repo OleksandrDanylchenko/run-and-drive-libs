9abab093cdb14b7563dd6ecc42d4259306ba2d8c
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-04-25 18:30:30 +0300

    feat: add initial database models

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,17 +1,79 @@
 export * from './functions';
 
-type UUID = string;
+export type UUID = string;
 
-export interface User {
+interface BaseModel {
   id: UUID;
-  name: string;
-  surname: string;
-  photoUrl: string;
-  email: string;
 }
 
-export interface Trip {
-  id: UUID;
-  startLocation: google.maps.LatLng;
-  timeStart: Date;
+export interface User extends BaseModel {
+  name: string;
+  surname: string;
+  email: string;
+  photoUrl: string;
+  phone: string;
+  refreshToken: string;
+}
+
+export interface Engineer extends User {
+  activationLogin: string;
+  employeeNumber: number;
+}
+
+export interface Emitter extends BaseModel {
+  engineerId: UUID;
+  activatedAt: string;
+}
+
+export interface Car extends BaseModel {
+  emitterId: UUID;
+  vin: string;
+  brand: string;
+  model: string;
+  year: string;
+  photosUrls: string[];
+  color: string;
+  mileage: number;
+  engine_capacity: number;
+  fuel_capacity: number;
+}
+
+export interface SensorRecord extends BaseModel {
+  carId: UUID;
+  tripId?: UUID;
+  tripStage?: 'start' | 'end';
+  fuelTankOccupancy: number;
+  wheelsPressure: {
+    frontLeft: number;
+    frontRight: number;
+    rearLeft: number;
+    rearRight: number;
+  };
+}
+
+export interface Trip extends BaseModel {
+  userId: UUID;
+  carId: UUID;
+  start: {
+    location: google.maps.LatLngLiteral;
+    time: string;
+  };
+  end?: {
+    location: google.maps.LatLngLiteral;
+    time: string;
+  };
+  totalDistance: number;
+  totalTime: number;
+}
+
+export interface TripMeasurement {
+  time: string;
+  location: google.maps.LatLngLiteral;
+}
+
+export interface TripBucket extends BaseModel {
+  tripId: UUID;
+  start_time: string;
+  end_time: string;
+  measurements: TripMeasurement[];
 }
