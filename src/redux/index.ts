c7d57fe996914c3508ecfd330c616632ec26acbc
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-05-12 18:11:30 +0300

    feat: add `getErrorMessage`

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,1 +1,19 @@
+import { SerializedError } from '@reduxjs/toolkit';
+import { FetchBaseQueryError } from '@reduxjs/toolkit/query/react';
+
+export const getErrorMessage = (
+  error?: FetchBaseQueryError | SerializedError,
+): string | undefined => {
+  if (!error) return;
+  if (!('status' in error)) return error.message;
+
+  let message: string;
+  if ('error' in error) {
+    message = error.error;
+  } else {
+    message = (error.data as { message: string }).message;
+  }
+  return `${error.status} ${message}`;
+};
+
 export * from './middlewares';
