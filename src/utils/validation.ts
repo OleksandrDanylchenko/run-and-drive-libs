fb0d1cf3d2c74f20fc1ee67c384eece894adae3d
OleksandrDanylchenko <olexander.danilchenko@gmail.com>
2022-05-14 12:27:41 +0300

    Add additional messages for the `required` validation

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,18 +1,21 @@
 import * as yup from 'yup';
 
-export const emailSchema = yup.string().email('Invalid email format').required();
+export const emailSchema = yup
+  .string()
+  .email('Invalid email format')
+  .required('Email is required');
 
 export const activationLoginSchema = yup
   .string()
   .length(8, 'Login should contain 8 characters')
-  .required();
+  .required('Login is required');
 
 export const carActivationCodeSchema = yup
   .string()
   .length(8, 'Car code should contain 8 characters')
-  .required();
+  .required('Car code is required');
 
 export const passwordSchema = yup
   .string()
   .min(8, 'Password should container at least 8 characters')
-  .required();
+  .required('Password is required');
